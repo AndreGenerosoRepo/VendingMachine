40e191f03bd94bcc778bee524c3eeb039ec29520
André Generoso <114500148+AndreGenerosoRepo@users.noreply.github.com>
2023-04-11 20:49:04 +0100

    start
    develop

--- src/main/java/com/challenge/challenge/ChallengeApplication.java
+++ src/main/java/com/challenge/challenge/ChallengeApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-
+//version1
 
 @SpringBootApplication
 public class ChallengeApplication {
